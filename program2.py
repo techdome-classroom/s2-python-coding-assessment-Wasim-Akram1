5a703f6aa2f647348e5a5d902040dae039f671c8
Wasim Akram <142140707+Wasim-Akram1@users.noreply.github.com>
2024-09-27 14:42:26 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -3,6 +3,7 @@ class Solution(object):
         roman={'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500,'M':1000}
         sum=0
         for i in range(len(s)-1,-1,-1):
+            num=roman[s[i]]
         """
         :type s: str
         :rtype: int
