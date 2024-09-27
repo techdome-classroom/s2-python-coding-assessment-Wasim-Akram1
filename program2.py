e17a2f33a6485bc5df451d0afef2d5fd138a71e4
Wasim Akram <142140707+Wasim-Akram1@users.noreply.github.com>
2024-09-27 14:40:50 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def romanToInt(self, s:str)->int:
-        roman={'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500 }
+        roman={'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500 'M':1000}
         """
         :type s: str
         :rtype: int
