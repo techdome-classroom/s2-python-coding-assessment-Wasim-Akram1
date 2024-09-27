37a904c00cf67f89b8b42113a65eec57963517c1
Wasim Akram <142140707+Wasim-Akram1@users.noreply.github.com>
2024-09-27 14:45:52 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def isValid(self, s:str)->bool:
-        """
-        :type s: str
-        :rtype: bool
-        """
         while True:
             if '()' in s:
                 s=s.replace('()','')
